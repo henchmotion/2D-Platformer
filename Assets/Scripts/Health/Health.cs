bf108ea0905b3b21db1ef1cd9d30d7c9ef092c8e
Henry Erondu <119769018+henchmotion@users.noreply.github.com>
2024-06-17 09:14:18 -0600

    iframes Settings
    Setting the iframes animations

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
-{
+{   
+    [Header ("Health")]
+   
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+     [Header("iframes")]
+     [SerializeField] private float iframesDuration;
+     [SerializeField] private int numberOfflashes;
+     private SpriteRenderer spriteRend;
+
     public void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();  
     }
 
     public void TakeDamage(float _damage)
@@ -22,7 +30,7 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             anim.SetTrigger("Hurt");
-            // iframes
+            StartCoroutine(Invunerability());
         }
         else
         {
@@ -35,5 +43,23 @@ public class Health : MonoBehaviour
             
         }
     }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
 
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+        for (int i = 0; i < numberOfflashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iframesDuration /(numberOfflashes *2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iframesDuration /(numberOfflashes *2));
+        }
+        
+         Physics2D.IgnoreLayerCollision(8, 9, false);
+    }
 }
+
